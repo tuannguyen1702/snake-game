54f0ca43d5840050d2fb580fd2bbec39e3398af8
TUAN NGUYEN <tuannguyen1702@gmail.com>
2025-03-28 17:00:57 +0700

    update - render snake & bait

--- src/components/SnakeGame.tsx
+++ src/components/SnakeGame.tsx
@@ -1,37 +1,86 @@
 import React, { useState } from "react";
 import "./SnakeGame.css";
 import GridSettingForm from "./GridSettingForm";
-import { GridSize } from "../types";
+import { GridSize, Position } from "../types";
 
 const GRID_W = 20;
 const GRID_H = 30;
 const CELL_SIZE = 20;
+const INITIAL_SNAKE: Position[] = [
+  { x: 2, y: 0 },
+  { x: 1, y: 0 },
+  { x: 0, y: 0 },
+];
 
 const SnakeGame: React.FC = () => {
   const [gridW, setGridW] = useState(GRID_W);
   const [gridH, setGridH] = useState(GRID_H);
   const [openSetting, setOpenSetting] = useState(false);
 
+  const [snake, setSnake] = useState<Position[]>(INITIAL_SNAKE);
+  const [bait, setBait] = useState<Position>(generateBait());
+
+  function generateBait(): Position {
+    return {
+      x: Math.floor(Math.random() * gridW),
+      y: Math.floor(Math.random() * gridH),
+    };
+  }
+
   const setGridSize = (gridSize: GridSize) => {
     setGridW(gridSize.width);
     setGridH(gridSize.height);
     setOpenSetting(false);
-  }
+  };
 
   return (
     <div className="game-container">
       <div className="game-header">
-          <button onClick={() => {setOpenSetting(true)}}>Setting</button>
+        <button
+          onClick={() => {
+            setOpenSetting(true);
+          }}
+        >
+          Setting
+        </button>
       </div>
       <div
         className="game-board"
         style={{
-          width: gridW * CELL_SIZE,
-          height: gridH * CELL_SIZE,
+          width: gridW * CELL_SIZE + 3,
+          height: gridH * CELL_SIZE + 3,
         }}
-      ></div>
+      >
+        <div className="snake-container">
+          {snake.map((position, index) => (
+            <div
+              key={index}
+              className="snake"
+              style={{
+                left: position.x * CELL_SIZE,
+                top: position.y * CELL_SIZE,
+              }}
+            />
+          ))}
+        </div>
 
-      {openSetting && <GridSettingForm gridWidth={gridW} gridHeight={gridH} onSubmit={setGridSize} onCancel={() => setOpenSetting(false)} />}
+        <div
+          className="bait"
+          style={{
+            left: bait.x * CELL_SIZE,
+            top: bait.y * CELL_SIZE,
+          }}
+        />
+      </div>
+
+      {openSetting && (
+        <GridSettingForm
+          gridWidth={gridW}
+          gridHeight={gridH}
+          onSubmit={setGridSize}
+          onCancel={() => setOpenSetting(false)}
+        />
+      )}
     </div>
   );
 };
--- src/types/index.ts
+++ src/types/index.ts
@@ -2,3 +2,8 @@ export type GridSize = {
     width: number;
     height: number;
 }
+
+export type Position = {
+    x: number;
+    y: number;
+  }
